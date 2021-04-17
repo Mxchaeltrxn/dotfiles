62b8784917471e9adc011207bbd1857ad745e236
Mxchaeltrxn <tranmichael97@gmail.com>
2021-04-17 10:54:40 +1000

    Add brew installations and vscode extensions

---
+++ brew.sh
@@ -0,0 +1,25 @@
+brew install \
+  git \
+  yarn \
+  youtube-dl \
+  terminal-notifier \
+  ffmpeg # Used with youtube-dl
+  make &&
+
+# GUI programs
+brew install --cask \
+  visual-studio \
+  omnidisksweeper \
+  visual-studio-code \
+  firefox \
+  rectangle \
+  iterm2 \
+  docker \
+  slack \
+  postgres \
+  postman \
+  anki \
+  tor-browser \
+  signal \
+  magic-wormhole \
+  spotify
